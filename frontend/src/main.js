ad7422f7f4ce64453b3bdeef333049ace95117db
kovacste <kovacste@bazisnet.hu>
2020-05-05 22:04:21 +0200

    kijelentkezse javitasa

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -21,10 +21,6 @@ const router = new VueRouter({ routes, mode: 'history' });
 router.beforeEach((to, from, next) => {
   const loggedIn = store.getters.user !== null || localStorage.getItem('username');
 
-  if(loggedIn && to.name === 'login') {
-    next('/home');
-  }
-
   if(!loggedIn && to.name !== 'login') {
     next('/login');
   } else {
